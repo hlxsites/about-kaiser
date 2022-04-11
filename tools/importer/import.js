880e60cf772521c79391edf2e75ab1c3b444c91c
David Nuescheler <david@nuescheler.ch>
2022-04-11 14:03:10 -0600

    chore: import caption

--- tools/importer/import.js
+++ tools/importer/import.js
@@ -10,97 +10,98 @@
  * governing permissions and limitations under the License.
  */
 /* eslint-disable no-console, class-methods-use-this */
+/* globals WebImporter */
 
 function createMetadata(document) {
-    const meta = {};
-  
-    const title = document.querySelector('title');
-    if (title) {
-      meta.Title = title.innerHTML.replace(/[\n\t]/gm, '');
-    }
-  
-    const desc = document.querySelector('[name="description"]');
-    if (desc) {
-      meta.Description = desc.content;
-    }
-  
-    const author = document.querySelector('[name="author"]');
-    if (author) {
-      meta.Author = author.content;
-    }
+  const meta = {};
 
-    const pubDate = document.querySelector('.eyebrow');
-    if (pubDate) {
-      meta['Publication Date'] = pubDate.textContent;
-      pubDate.remove();
-    }
-  
-    const tags = document.querySelector('.c21-tags');
-    if (tags) {
-        meta['Tags'] = [...tags.querySelectorAll('a')].map((a) => a.textContent).join(', ');
-        tags.remove();
-    }      
-  
-    const block = WebImporter.Blocks.getMetadataBlock(document, meta);
-    document.body.append(block);
-  
-    return meta;
+  const title = document.querySelector('title');
+  if (title) {
+    meta.Title = title.innerHTML.replace(/[\n\t]/gm, '');
   }
 
-  const makeGlobal = (href) => {
-    return new URL(href, 'https://about.kaiserpermanente.org/').href;
+  const desc = document.querySelector('[name="description"]');
+  if (desc) {
+    meta.Description = desc.content;
   }
 
-  const createRelated = (document) => {
-    const related = document.querySelector('.related-cards__wrap');
-    if (related) {
-      const data = [['Related Articles']];
-      const text = [...related.querySelectorAll('a.related-cards__card')].map((a) => `<p><a href="${makeGlobal(a.href)}">${makeGlobal(a.href)}</a></p>`).join('\n');
-      data.push([text]);
-  
-      const table = WebImporter.DOMUtils.createTable(data, document);
-      related.replaceWith(table);
-    }
-  };
-  
-  const createHeroImage = (document) => {
-    const hero = document.querySelector('.c27-article-page-hero__media-item');
-    if (hero) {
-      const url = hero.style.backgroundImage.split('(')[1].replace(')','');;
-      hero.innerHTML = `<img src="${url}">`;
-    }
-  };
+  const author = document.querySelector('[name="author"]');
+  if (author) {
+    meta.Author = author.content;
+  }
+
+  const pubDate = document.querySelector('.eyebrow');
+  if (pubDate) {
+    meta['Publication Date'] = pubDate.textContent;
+    pubDate.remove();
+  }
+
+  const tags = document.querySelector('.c21-tags');
+  if (tags) {
+    meta.Tags = [...tags.querySelectorAll('a')].map((a) => a.textContent).join(', ');
+    tags.remove();
+  }
+
+  const block = WebImporter.Blocks.getMetadataBlock(document, meta);
+  document.body.append(block);
+
+  return meta;
+}
+
+const makeGlobal = (href) => new URL(href, 'https://about.kaiserpermanente.org/').href;
+
+const createRelated = (document) => {
+  const related = document.querySelector('.related-cards__wrap');
+  if (related) {
+    const data = [['Related Articles']];
+    const text = [...related.querySelectorAll('a.related-cards__card')].map((a) => `<p><a href="${makeGlobal(a.href)}">${makeGlobal(a.href)}</a></p>`).join('\n');
+    data.push([text]);
+
+    const table = WebImporter.DOMUtils.createTable(data, document);
+    related.replaceWith(table);
+  }
+};
+
+const createHeroImage = (document) => {
+  const hero = document.querySelector('.c27-article-page-hero__media-item');
+  if (hero) {
+    const url = hero.style.backgroundImage.split('(')[1].replace(')', '');
+    const caption = document.querySelector('.c27-article-page-hero__caption');
+    const captionText = caption ? caption.textContent : '';
+    hero.parentElement.parentElement.innerHTML = `<img src="${url}"><p><em>${captionText}</em></p>`;
+  }
+};
 
 export default {
 
-    /**
-     * Apply DOM operations to the provided document and return
-     * the root element to be then transformed to Markdown.
-     * @param {HTMLDocument} document The document
-     * @returns {HTMLElement} The root element
-     */
-    transformDOM: ({ document, url }) => {
-      // simply return the body, no transformation (yet)
-      const remove = document.querySelectorAll('.header, .footer, .breadcrumb, .social-share, .c27-article-page-hero__text');
-      if (remove) {
-        remove.forEach((element) => { element.remove(); });
-      }
+  /**
+   * Apply DOM operations to the provided document and return
+   * the root element to be then transformed to Markdown.
+   * @param {HTMLDocument} document The document
+   * @returns {HTMLElement} The root element
+   */
+  // eslint-disable-next-line no-unused-vars
+  transformDOM: ({ document, url }) => {
+    // simply return the body, no transformation (yet)
+    const remove = document.querySelectorAll('.header, .footer, .breadcrumb, .social-share, .c27-article-page-hero__text');
+    if (remove) {
+      remove.forEach((element) => { element.remove(); });
+    }
 
-      createHeroImage(document);
+    createHeroImage(document);
 
-      createMetadata(document);
-      createRelated(document);
+    createMetadata(document);
+    createRelated(document);
 
-      return document.body;
-    },
-  
-    /**
-     * Return a path that describes the document being transformed (file name, nesting...).
-     * The path is then used to create the corresponding Word document.
-     * @param {String} url The url of the document being transformed.
-     * @param {HTMLDocument} document The document
-     */
-    generateDocumentPath: ({ document, url }) => {
-      return new URL(url).pathname.replace(/\/$/, '');
-    },
-  }
+    return document.body;
+  },
+
+  /**
+   * Return a path that describes the document being transformed (file name, nesting...).
+   * The path is then used to create the corresponding Word document.
+   * @param {String} url The url of the document being transformed.
+   * @param {HTMLDocument} document The document
+   */
+  // eslint-disable-next-line no-unused-vars
+  generateDocumentPath: ({ document, url }) => new URL(url).pathname.replace(/\/$/, ''),
+};
